b56793595fc76b1cbe88ef9fd38a9ffe143df79c
DivyanshuArya <aryadivyanshu29@gmail.com>
2025-03-04 23:36:06 +0530

    Adding Navbar2

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -13,10 +13,7 @@ import Footer from './Footer';
 const Navbar2 = () =>{
     return(
         <>
-        <div className='gradiant-background relative w-screen h-screen bg-[#eae4e4] overflow-hidden'>
-            <div className='absolute top-0 left-2 w-96 h-96 bg-gradient-to-br from-[#6EEFC86B] from-46% via-[#48C9D7B0] from-69.1% to-[#1C9DE8] opacity-50 blur-3xl rounded-full'></div>
-            <div className='absolute bottom-10 right-20 w-96 h-96 bg-gradient-to-br from-[#6EEFC86B] from-46% via-[#48C9D7B0] from-20% to-[#1C9DE8] opacity-50 blur-3xl rounded-full'></div>
-            <div className='m-5 h-[70px] rounded-xl items-center flex bg-green-400'>
+            <div className='m-5 h-[70px] rounded-xl items-center flex bg-[#00C98D]'>
             
             <Image className="mt-7" 
             src={Logo}
@@ -41,7 +38,6 @@ const Navbar2 = () =>{
             alt="location" />
 
             </div>
-        </div>
         </>
         
     );
